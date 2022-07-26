459bb0c3a77473c15e9b7ae317a54fcb3f57155d
HELL-BOY <107465084+HELL-BOY77@users.noreply.github.com>
2022-07-26 18:13:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const chalk = require("chalk")
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
+global.autoReadAll = true // auto reading in all pchat gc and status (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
